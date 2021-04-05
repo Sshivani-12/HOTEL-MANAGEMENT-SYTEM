956ad2ca7e58bccc7c02910176894987c434e942
Sshivani-12 <shivaniwakde1211@gmail.com>
2021-04-05 22:55:41 +0530

    started with log in page

--- src/hotel/management/HOTELMANAGEMENT.java
+++ src/hotel/management/HOTELMANAGEMENT.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package hotel.management;
+import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -16,18 +17,58 @@ public class HOTELMANAGEMENT extends JFrame {
     //setSize(400,400);
     //setLocation(300,300);
     
-   ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("hotel/management/home.jpg")); 
+/*
+ IMAGE TO BE ADDED
+    
+ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("hotel/management/icons/home.jpg")); 
 //ImageIcon to take image
     
-    JLabel l1=new JLabel(i1);//image la label madhe takaycha
+JLabel l1=new JLabel(i1);//image la label madhe takaycha
+l1.setBounds(0,0,1366,565); //y axis,x axis,size
+    add(l1);
     
+    */
+    JLabel l1=new JLabel("");//image la label madhe takaycha
+   
     l1.setBounds(0,0,1366,565); //y axis,x axis,size
     add(l1);
-
+    
+    JLabel l2=new JLabel("Hotel Management System");//image la label madhe takaycha
+   
+    l2.setBounds(300,150,900,90); //y axis,x axis,size
+    l2.setForeground(Color.BLACK);//in awt packet
+    l2.setFont(new Font("serif",Font.PLAIN,70));
+    l1.add(l2);
+    
+    JButton b1=new JButton("Next");
+    b1.setBackground(Color.BLACK);
+    b1.setForeground(Color.WHITE);
+    b1.setBounds(580,390,150,50);
+    l1.add(b1);
+    
+    setLayout(null);
+    setVisible(true);
+    
+    while(true)
+    {
+        l2.setVisible(false);
+      
+        try{
+        
+            Thread.sleep(500);
+        }
+        catch(Exception e){}
+     l2.setVisible(true);
+       
+        try{
+        Thread.sleep(500);
+        }
+        catch(Exception e){}
+                
+    }
+    
+    
  }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
   new HOTELMANAGEMENT();
---
+++ src/hotel/management/Login.java
@@ -0,0 +1,16 @@
+
+package hotel.management;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+
+public class Login extends JFrame{
+    
+    
+    Login()
+    {
+     JTextField t1=new JTextField();   
+    }
+}
